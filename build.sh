5c8dcccdaa8868dc5b8ca6d475ee4965fc3ff24e
James Richardson <james@time4tea.net>
2017-03-10 09:46:29 +0000

    update build so it may automatically promote to nexus, when the artifacts can be signed.

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-$(dirname $0)/gradlew test uploadArchives closeAndPromoteRepository
+BASE_VERSION=0.1
+BUILD_VERSION="1-SNAPSHOT"
+RELEASE=no
+ADDITIONAL_TASKS=""
+
+# codeshop magic beans
+if [ ! -z "$CI_BUILD_NUMBER" ]
+then
+    BUILD_VERSION=$CI_BUILD_NUMBER
+    RELEASE="yes"
+fi
+
+VERSION=${BASE_VERSION}.${BUILD_VERSION}
+
+if [ $RELEASE = "yes" ]
+then
+    ADDITIONAL_TASKS="uploadArchives closeAndPromoteRepository"
+fi
+
+$(dirname $0)/gradlew -PVERSION=${VERSION} test ${ADDITIONAL_TASKS}
+
 
 
